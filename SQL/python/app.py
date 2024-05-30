b95ee3eba3590c72e743802ec8130df565d69890
AloHerdz <132958470+AloHerdz@users.noreply.github.com>
2024-05-30 12:21:29 -0600

    Update app.py

--- SQL/python/app.py
+++ SQL/python/app.py
@@ -2,35 +2,64 @@ import streamlit as st
 import mysql.connector
 import pandas as pd
 
-# Leer las credenciales de secrets.toml
+# Configuración de la conexión a la base de datos
 def get_connection():
-    connection = mysql.connector.connect(
+    return mysql.connector.connect(
         host=st.secrets["mysql"]["host"],
+        port=3306,
         user=st.secrets["mysql"]["user"],
         password=st.secrets["mysql"]["password"],
         database=st.secrets["mysql"]["database"]
     )
-    return connection
 
-# Función para ejecutar consultas
-def run_query(query):
-    connection = get_connection()
-    cursor = connection.cursor()
-    cursor.execute(query)
-    result = cursor.fetchall()
-    cursor.close()
-    connection.close()
-    return result
+# Función para ejecutar la consulta y obtener los datos
+def fetch_data():
+    try:
+        connection = get_connection()
+        db_info = connection.get_server_info()
+        st.write(f"Connected to MySQL server version {db_info}")
+
+        cursor = connection.cursor()
+        query = "SELECT DATABASE();"
+        cursor.execute(query)
+        record = cursor.fetchone()
+        st.write(f"You are connected to MySQL Database: {record[0]}")  # Acceder al primer elemento de la tupla
+
+        # Realiza la consulta adicional
+        query = '''
+            SELECT libro_nombre, 
+                   libro_autor, 
+                   libro_editorial, 
+                   libro_ano_publicacion, 
+                   libro_genero, 
+                   libro_precio 
+            FROM libros_table
+            WHERE libro_genero = %s
+        '''
+        
+        # Ejecuta la consulta usando pandas
+        df = pd.read_sql(query, con=connection, params=('Ficción', ))
+        return df
+
+    except mysql.connector.Error as err:
+        st.error(f"Error: {err}")
+        return None
 
 # Interfaz de usuario con Streamlit
 st.title("Sistema de Gestión de Libros")
 
-st.header("Datos de la Base de Datos")
+df = fetch_data()
+if df is not None:
+    st.write("Descripción de los datos:")
+    st.write(df.describe().applymap('{:,.2f}'.format))
 
-# Ejecutar una consulta de ejemplo
-query = "SELECT * FROM libros"  # Reemplaza con tu consulta
-data = run_query(query)
+    genero_filter = df['libro_genero'] == 'Ficción'
+    st.write("Datos filtrados por género 'Ficción':")
+    st.write(df[genero_filter].describe().applymap('{:,.2f}'.format))
 
-# Mostrar los datos en una tabla
-df = pd.DataFrame(data, columns=['ID', 'Nombre', 'Autor', 'Editorial', 'Año'])  # Ajusta las columnas según tu tabla
-st.write(df)
+    st.write("Tabla completa de datos:")
+    st.write(df)
+
+else:
+    st.error("Error connecting to the database.")
+
